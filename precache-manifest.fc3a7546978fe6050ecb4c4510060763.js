f37121f3add7d539e7cd542872d9898df5d76e1f
Marcus <brooksomarcus@gmail.com>
2018-10-12 18:14:11 -0400

    Updates

---
+++ precache-manifest.fc3a7546978fe6050ecb4c4510060763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resumeSite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c48c013b6f02ba54ff00",
+    "url": "/resumeSite/static/js/runtime~main.c48c013b.js"
+  },
+  {
+    "revision": "c5c95ba4755a71401d42",
+    "url": "/resumeSite/static/js/main.c5c95ba4.chunk.js"
+  },
+  {
+    "revision": "2edf3ad1d390dc1165a6",
+    "url": "/resumeSite/static/js/1.2edf3ad1.chunk.js"
+  },
+  {
+    "revision": "c5c95ba4755a71401d42",
+    "url": "/resumeSite/static/css/main.af45965f.chunk.css"
+  },
+  {
+    "revision": "10da3d45ee6bb94266030ee8693814c4",
+    "url": "/resumeSite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1d6d7cac67d1f8f667b37579fb04614.js"
+  "/resumeSite/precache-manifest.fc3a7546978fe6050ecb4c4510060763.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/resumeSite/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
