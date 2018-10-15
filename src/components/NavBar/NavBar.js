f98ded7b4d9d0bdf38737d50ee7b8358cd7213a1
Marcus <brooksomarcus@gmail.com>
2018-10-15 19:06:14 -0400

    Adding resume to site

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,16 +8,16 @@ const NavBar = ({routeChange}) => {
 				<ul>
 					<li onClick={routeChange.bind('','aboutMe')} className="phoneAboutMe">About Me</li>
 					<li onClick={routeChange.bind('', 'portfolio')} className="phonePortfolio">Portfolio</li>
-					<li onClick={routeChange.bind('', 'resume')} className="phoneResume">Resume</li>
+					<li className="phoneResume"><a href={require("../../images/Resume.pdf")}>Resume</a></li>
 					<li onClick={routeChange.bind('', 'contactMe')} className="phoneContactMe">Contact Me</li>
 				</ul>
 			</nav>
 			<nav className="desktopNav">
 				<ul>
-					<li onClick={routeChange.bind('','aboutMe')} className="desktopAboutMe">About Me</li>
+					<li onClick={routeChange.bind('', 'aboutMe')} className="desktopAboutMe">About Me</li>
 					<li onClick={routeChange.bind('', 'portfolio')} className="desktopPortfolio">Portfolio</li>
 					<img className="logo" src={require("../../images/Logo.png")}/>
-					<li onClick={routeChange.bind('', 'resume')} className="desktopResume">Resume</li>
+					<li className="desktopResume"><a href={require("../../images/Resume.pdf")}>Resume</a></li>
 					<li onClick={routeChange.bind('', 'contactMe')} className="desktopContactMe">Contact Me</li>
 				</ul>
 			</nav>
