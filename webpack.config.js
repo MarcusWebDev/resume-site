0a85ed383df9cb01f8eeeb7cda662f93864241e2
marcuswebdev <marcusbrookswebdev@gmail.com>
2025-08-12 16:58:31 -0400

    Moved index.html to src directory

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dist/index.html',
+      template: './src/index.html',
       favicon: './public/favicon.ico'
     }),
   ],
