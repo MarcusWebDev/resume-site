ee32a0f61748a6716ef7832a905b741491c404da
marcuswebdev <marcusbrookswebdev@gmail.com>
2025-09-08 17:39:30 -0400

    Updated the projects section to include links to the live application in the project header, and links to github beneath the description. Also increased the opacity slightly on the background video filter to help with readability.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,22 +5,42 @@ const Projects = () => {
     <div className="Projects">
       <div className="projects-content-container">
         <h1>Projects</h1>
-        <h2>The Amiibo Library</h2>
-        <p>
-          <a href="https://amiibolibrary.com/" className="projects-link">
-            The Amiibo Library
-          </a>{" "}
-          was made to provide users with information on amiibos and help them
-          manage their collection.
-        </p>
-        <h2>Rainbow Angles</h2>
-        <p>
-          <a href="https://rainbowangles.com/#/" className="projects-link">
-            Rainbow Angles
-          </a>{" "}
-          is a website created to help players of the video game Rainbow Six
-          Siege improve their knowledge of the game.
-        </p>
+        <div className="project-container">
+          <a href="https://amiibolibrary.com/">
+            <h2>The Amiibo Library</h2>
+          </a>
+          <p>
+            The Amiibo Library was made to provide users with information on
+            amiibos and help them manage their collection.
+          </p>
+          <a
+            className="github-link"
+            href="https://github.com/MarcusWebDev/the-amiibo-library"
+          >
+            Github - Front-End
+          </a>
+          <a
+            className="github-link"
+            href="https://github.com/MarcusWebDev/the-amiibo-library-api"
+          >
+            Github - Back-End
+          </a>
+        </div>
+        <div className="project-container">
+          <a href="https://rainbowangles.com/#/">
+            <h2>Rainbow Angles</h2>
+          </a>
+          <p>
+            Rainbow Angles is a website created to help players of the video
+            game Rainbow Six Siege improve their knowledge of the game.
+          </p>
+          <a
+            className="github-link"
+            href="https://github.com/MarcusWebDev/rainbow-angles"
+          >
+            Github
+          </a>
+        </div>
       </div>
     </div>
   );
